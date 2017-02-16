eb8c5492c54588a3c2c9e02d07ffac03ff817ccb
Bobby Montalvo <bobby.montalvo@gmail.com>
2017-02-16 01:06:51 -0500

    Initial commit.

---
+++ AWSRedisDemo/Controllers/GeoPerformancesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AWSRedisDemo.Controllers
+{
+    public class GeoPerformancesController : ApiController
+    {
+        // GET: api/GeoPerformances
+        public IEnumerable<string> Get()
+        {
+            var host = ConfigurationManager.AppSettings["RedisHost"];
+            var service = new GeoService(host);
+            var toReturn = service.GetValues();
+
+            return toReturn;
+        }
+    }
+}
---
+++ AWSRedisDemo/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using StackExchange.Redis;
+using StackExchange.Redis.Extensions.Core;
+using StackExchange.Redis.Extensions.Newtonsoft;
+using System.Collections.Generic;
+
+[assembly: OwinStartup(typeof(AWSRedisDemo.Startup))]
+
+namespace AWSRedisDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute("DefaultAPI",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            app.UseWebApi(config);
+        }
+    }
+
+    public class Dummy
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class GeoService
+    {
+        private readonly IConnectionMultiplexer _redis;
+        private readonly ICacheClient _client;
+
+        public GeoService(string host)
+        {
+            _redis = ConnectionMultiplexer.Connect(host);
+
+            var serializer = new NewtonsoftSerializer();
+            _client = new StackExchangeRedisCacheClient(_redis, serializer);
+
+            Seed();
+        }
+
+        public void Seed()
+        {
+            var dummy = new Dummy() { Id = "bob", Message = "Writes all the data here." };
+           _client.Add<Dummy>(dummy.Id, dummy);
+        }
+
+        public List<string> GetValues()
+        {
+            var toReturn = new List<string>();
+            var result = _client.Get<Dummy>("bob");
+
+            toReturn.Add(result.Message);
+
+            return toReturn;
+        }
+    }
+}
